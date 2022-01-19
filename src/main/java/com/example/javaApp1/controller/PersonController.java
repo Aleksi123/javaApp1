73034dd402024db7ffd6ffbe80b960bb70f23767
Aleksi <a12345@live.fi>
2022-01-19 22:15:43 +0200

    Created Controller classes
    - PersonController, AddressController, PersonInfoController

---
+++ src/main/java/com/example/javaApp1/controller/PersonController.java
@@ -0,0 +1,14 @@
+package com.example.javaApp1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path="/api")
+public class PersonController {
+
+}
---
+++ src/main/java/com/example/javaApp1/controller/PersonInfoController.java
@@ -0,0 +1,9 @@
+package com.example.javaApp1.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/api")
+public class PersonInfoController {
+
+}
